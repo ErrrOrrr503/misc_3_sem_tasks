09ec44e45cd1cf7078513fb9dc86866ceb52556e
errrorrr503 <egortit-33@yandex.ru>
2020-10-29 01:44:01 +0300

    17_msg_board server.c: 	got rid of global pointers to free and exit from signal handler. Switched to sigaction.

--- 17_msg_board/server.c
+++ 17_msg_board/server.c
@@ -11,9 +11,7 @@
 
 extern int errno;
 
-volatile mqd_t g_mq = 0; // other way of allowing sig_handler to see it?
-volatile char *g_mq_name = NULL;
-volatile char **g_buf_to_free = NULL;
+volatile char flag_termination = 0;
 
 char * check_mq_name_alloc (const char *raw_name);
 ssize_t mq_receive_alloc (mqd_t mq, char **buf, unsigned *prio_received);
@@ -25,31 +23,45 @@ int main (int argc, char *argv[])
         printf ("usage: '%s' <msg_queue_name>\n", argv[0]);
         return 0;
     }
-    signal (SIGINT, sig_handler);
-    signal (SIGTERM, sig_handler);
-
+    struct sigaction act = {0};
+    act.sa_handler = sig_handler; //new handler   
+        // do not need sa_sigaction (used with SA_SIGINFO)
+        // do not need to block other signals
+        // do not need special flags
+        // sa_restorer not even in posix
+    int ret = sigaction (SIGINT, &act, NULL);
+    ret |= sigaction (SIGTERM, &act, NULL);
+    if (ret) {
+        perror ("sigacrion error");
+        return -1;
+    }
     //form mq_name, as it must be '/somename' 
     char *mq_name = check_mq_name_alloc (argv[1]);
-    g_mq_name = mq_name;
     if (mq_name == NULL)
         return -1;
     printf ("formed mq_name is: '%s'\n", mq_name);
     //create queue
     mqd_t mq = mq_open (mq_name, O_RDONLY | O_CREAT | O_EXCL, 0644, NULL); // 0644 :rdwr for user (and server process) wronly for others. NULL: default params
-    g_mq = mq;
     if (mq == -1) {
         perror ("can't open or create msg_queue");
+        free (mq_name);
         return -1;
     }
     // receive msg
     while (1) {
         unsigned prio = 0;
         char *buf = NULL;
-        g_buf_to_free = (volatile char **) &buf;
         ssize_t msg_sz = mq_receive_alloc (mq, &buf, &prio);// why alloc? commonly, nobody guarants that queue size is const, here it is const
-        if (msg_sz == -1) {
+        if (msg_sz == -1 && !flag_termination) {
+            perror ("can't receive msg");
+            flag_termination = 1;
+        }
+        if (flag_termination) {
+            mq_close (mq);
+            mq_unlink (mq_name);
             if (buf != NULL)
                 free (buf);
+            free (mq_name);
             return -1;
         }
         //print msg
@@ -74,12 +86,12 @@ char * check_mq_name_alloc (const char *raw_name)
     }
     size_t raw_name_len = i;
     //check length
-    if (raw_name_len > NAME_MAX) {
+    if (raw_name_len + 1 > NAME_MAX) { // +1 to include '\0'
         printf ("queue name too long, read man 7 mq_overview\n");
         return NULL;
     }
     //form correct name
-    char *mq_name = (char *) calloc (raw_name_len + 1, sizeof (char));
+    char *mq_name = (char *) calloc (raw_name_len + 2, sizeof (char)); // +1 for maybe '/' +1 for '\0'
     if (mq_name == NULL) {
         printf ("can't allocate memory for mq_name\n");
         return NULL;
@@ -96,17 +108,14 @@ ssize_t mq_receive_alloc (mqd_t mq, char **buf, unsigned *prio_received)
 {
     struct mq_attr mq_attributes = {0};
     if (mq_getattr (mq, &mq_attributes)) {
-        perror ("can't get msg_queue attributes");
         return -1;
     }
     *buf = (char *) calloc (mq_attributes.mq_msgsize + 1, sizeof (char)); // +1 for '\0'
     if (*buf == NULL) {
-        perror ("can't alloc memory");
         return -1;
     }
     ssize_t msg_sz = mq_receive (mq, *buf, (size_t) mq_attributes.mq_msgsize, prio_received);
     if (msg_sz == -1) {
-        perror ("can't receive msg");
         return -1;
     }
     return msg_sz;
@@ -114,10 +123,6 @@ ssize_t mq_receive_alloc (mqd_t mq, char **buf, unsigned *prio_received)
 
 void sig_handler (int sig)
 {
-    mq_close ((mqd_t) g_mq);
-    mq_unlink ((char *) g_mq_name);
-    free ((void *) *g_buf_to_free);
-    free ((void *) g_mq_name);
+    flag_termination = 1;
     printf ("\nterminated by signal: %d : %s\n", sig, strsignal (sig));
-    exit (0);
 }
